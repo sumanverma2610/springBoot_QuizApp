85efdd64b2689fe20abd3449906d1e03d6e18226
suman verma <sumanverma26102004@gmail.com>
2026-08-04 15:23:39 +0530

    Question Entity Created

---
+++ src/main/java/com/Quiz/QuizApplication/entity/Question.java
@@ -0,0 +1,104 @@
+package com.Quiz.QuizApplication.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "questions")
+public class Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String question;
+
+    @Column(nullable = false)
+    private String option1;
+
+    @Column(nullable = false)
+    private String option2;
+
+    @Column(nullable = false)
+    private String option3;
+
+    @Column(nullable = false)
+    private String option4;
+
+    @Column(nullable = false)
+    private String correctAnswer;
+
+    // Default Constructor
+    public Question() {
+    }
+
+    // Parameterized Constructor
+    public Question(Long id, String question, String option1, String option2,
+                    String option3, String option4, String correctAnswer) {
+        this.id = id;
+        this.question = question;
+        this.option1 = option1;
+        this.option2 = option2;
+        this.option3 = option3;
+        this.option4 = option4;
+        this.correctAnswer = correctAnswer;
+    }
+
+    // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getOption1() {
+        return option1;
+    }
+
+    public void setOption1(String option1) {
+        this.option1 = option1;
+    }
+
+    public String getOption2() {
+        return option2;
+    }
+
+    public void setOption2(String option2) {
+        this.option2 = option2;
+    }
+
+    public String getOption3() {
+        return option3;
+    }
+
+    public void setOption3(String option3) {
+        this.option3 = option3;
+    }
+
+    public String getOption4() {
+        return option4;
+    }
+
+    public void setOption4(String option4) {
+        this.option4 = option4;
+    }
+
+    public String getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    public void setCorrectAnswer(String correctAnswer) {
+        this.correctAnswer = correctAnswer;
+    }
+}
